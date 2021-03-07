91cad70d6b58493a663307cfdfb93570181aa592
Masato NUNOKAWA <masato.nunokawa@gmail.com>
2021-03-07 15:45:22 +0900

    実験コードとコメント追加

--- src/X/libX.cc
+++ src/X/libX.cc
@@ -5,12 +5,14 @@ X::greeting(void) {
 
   // 意図的に memory leakを仕込んでみる
   //  char *str = new char[8]; // buffer overrunは '-fsanitize=address'で検出される
-  char *str = new char[256]; // こちらだとエラーが起きない (アドレスの汚染がないので)
+
+  char *str = new char[256]; // こちらだとエラーが起きない (アドレスの汚染がないので?)
 
   sprintf(str, "Hello World");
   std::string retVal(str);
 
   //delete[] str;
+  //delete[] str; // double freeは検出可能
 
   return str;
 }
